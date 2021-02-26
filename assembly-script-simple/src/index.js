cd603e55846f75053630d6b7d651f5fc237402a4
alex-greff <alex.j.greff@gmail.com>
2021-02-26 10:39:30 -0500

    Changed module instantiation to use AssemblyScript Loader

--- assembly-script-simple/src/index.js
+++ assembly-script-simple/src/index.js
@@ -3,22 +3,28 @@
     console.log("Loading WASM Module....");
 
     // Instantiate the WebAssembly module
-    const wasmModule = await WebAssembly.instantiateStreaming(
+    // window.loader is the AssemblyScript loader which was loaded in index.html
+    // This loader is similar to WebAssembly.instantiateStreaming except it
+    // adds additional functions onto wasmModule.instance.exports for 
+    // managing/interacting with the WASM module's memory from JavaScript
+    const wasmModule = await window.loader.instantiate(
       fetch("./build/optimized.wasm"),
-      {
-        env: {
-          abort(_msg, _file, line, column) {
-            console.error("abort called at index.ts:" + line + ":" + column);
-          }
-        },
-      }
     );
+    // Here is how you would instantiate the module normally, without the
+    // AssemblyScript helper functions being inject 
+    // const wasmModule = await WebAssembly.instantiateStreaming(
+    //   fetch("./build/optimized.wasm"),
+    // );
 
     console.log("WASM Module Loaded!");
-    console.log("Module", wasmModule); // TODO: remove
+
+    // You can check out what the module looks with the following
+    console.log("Module", wasmModule);
 
     // --- Sum Example ---
 
+    // Handle submission of the sum example form, running the add function
+    // in our WASM module
     const sumExampleForm = document.querySelector("#sum-example");
     sumExampleForm.addEventListener("submit", (event) => {
       event.preventDefault();
